b6fdc905a02d3236c9b064faaba24c56b9070470
maverick.woo <maverick.woo@9a7d1bda-602f-11de-bbaa-f1a44a2b9d47>
2009-06-23 21:23:56 +0000

    initial import

---
+++ trunk/buildall.bat
@@ -0,0 +1,11 @@
+perl build.pl samples/sms.c "C" "c"
+perl build.pl samples/hl-line.el "Emacs Lisp" "el"
+perl build.pl samples/GetEnv.java "Java" "java"
+perl build.pl samples/test.tex "LaTeX" "tex"
+perl build.pl samples/csvformat.pl "Perl" "pl"
+
+dir html\*-c.html | tail -n 3
+dir html\*-el.html | tail -n 3
+dir html\*-java.html | tail -n 3
+dir html\*-tex.html | tail -n 3
+dir html\*-pl.html | tail -n 3
